2a05ffc652dee277ba8b115aa40eb7fab32feaf3
Aaron Hahsbrown <aaron.social2099@gmail.com>
2025-09-25 20:24:17 -0600

    completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,13 @@
 using System;
+// did this for fun 
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+
+        fjfjfjfjfjfjfj ;
     }
 }
